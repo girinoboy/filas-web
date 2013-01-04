59cf494bdc23d0f9913779bf596b88beab27a79b
girinoboy <girinoboy@4a19f9eb-a919-de0c-50ec-919b7c35c86e>
2013-01-04 18:44:58 +0000

    salvando no banco, fala testar;

--- trunk/OrenPS/src/br/com/utility/Constantes.java
+++ trunk/OrenPS/src/br/com/utility/Constantes.java
@@ -4,6 +4,7 @@ public class Constantes {
 	public static final String ID_PERFIL_ADMINISTRADOR = "1";
 	public static final CharSequence PAGINA_EXIBIR_PERFIL = "index.xhtml";
 	public static final CharSequence PAGINA_EXIBIR_USUARIO = "index.xhtml";
+	public static final Long ID_PERIL_PADRAO = 3l;
 	public static String PAGINA_INDEX = "login.xhtml";
 
 }
