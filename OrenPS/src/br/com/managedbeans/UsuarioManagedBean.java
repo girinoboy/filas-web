b92e388245128f3287a8f4b01f30f3177f32f120
girinoboy <girinoboy@4a19f9eb-a919-de0c-50ec-919b7c35c86e>
2013-01-04 17:55:09 +0000

    para cadastro de usuario.

---
+++ OrenPS/src/br/com/managedbeans/UsuarioManagedBean.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package br.com.managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ * @author marcleonio.medeiros
+ *
+ */
+@ManagedBean
+@ViewScoped
+public class UsuarioManagedBean {
+
+	/**
+	 * 
+	 */
+	public UsuarioManagedBean() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public void saveUsuario(){
+		
+		
+	}
+
+}
