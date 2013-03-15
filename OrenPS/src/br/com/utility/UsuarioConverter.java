101f886276eb55fe25b9b3ef22ffb5bee2216b0a
girinoboy <girinoboy@4a19f9eb-a919-de0c-50ec-919b7c35c86e>
2013-03-15 20:47:26 +0000

    converter de usuario

---
+++ OrenPS/src/br/com/utility/UsuarioConverter.java
@@ -0,0 +1,61 @@
+package br.com.utility;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+
+import br.com.dao.UsuarioDAO;
+import br.com.models.Usuario;
+  
+@FacesConverter(value="usuario")
+public class UsuarioConverter implements Converter {  
+  
+    public static List<Usuario> usuarioBD;  
+    
+    static UsuarioDAO usuarioDAO = new UsuarioDAO();
+  
+    static {  
+        usuarioBD = new ArrayList<Usuario>();  
+        try {
+			usuarioBD = usuarioDAO.list();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }  
+  
+    public Object getAsObject(FacesContext facesContext, UIComponent component, String submittedValue) {  
+        if (submittedValue.trim().equals("")) {  
+            return null;  
+        } else {  
+            try {  
+                int number = Integer.parseInt(submittedValue);  
+  
+                for (Usuario p : usuarioBD) {  
+                    if (p.getId() == number) {  
+                        return p;  
+                    }  
+                }  
+  
+            } catch(NumberFormatException exception) {  
+                throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Conversion Error", "Not a valid player"));  
+            }  
+        }  
+  
+        return null;  
+    }  
+  
+    public String getAsString(FacesContext facesContext, UIComponent component, Object value) {  
+        if (value == null || value.equals("")) {  
+            return "";  
+        } else {  
+            return String.valueOf(((Usuario) value).getId());  
+        }  
+    }  
+}  
