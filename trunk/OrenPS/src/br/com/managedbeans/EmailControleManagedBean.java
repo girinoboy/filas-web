539030b78d93be461efc20a3c5d94e806eb5c5f4
girinoboy <girinoboy@4a19f9eb-a919-de0c-50ec-919b7c35c86e>
2013-03-20 05:40:40 +0000

    enviando email

--- trunk/OrenPS/src/br/com/managedbeans/EmailControleManagedBean.java
+++ trunk/OrenPS/src/br/com/managedbeans/EmailControleManagedBean.java
@@ -62,9 +62,9 @@ public class EmailControleManagedBean {
 	private static EmailBean emailBean = new EmailBean();
 
 	private AnexoDAO anexoDAO = new AnexoDAO();
-	
+
 	private List<Questionario> questionariosSelecionados;
-	
+
 	private List<Usuario> usuariosSelecionados;
 
 	static {  
@@ -113,192 +113,197 @@ public class EmailControleManagedBean {
 	} 
 
 	public void enviarEmail(){
-		Object a = questionariosSelecionados;
 		
-		a = usuariosSelecionados;
 		emailBean.setSubject("Pesquisa de satisfação");
 		emailBean.setMsg("This is a test mail ... :-)");
-		emailBean.setTo("girinoboy@gmail.com");
-		
 		//enviaEmail(emailBean);
 		EmailUtils emailUtils = new EmailUtils();
+		Anexo anexo = new Anexo();
 		try {
-			Anexo anexo = new Anexo();
 			//pesquisar pela img recebendo o parametro pelo tipo de questionario
 			//cada questionario pode ter uma arte de apresentração diferente para ela
 			//mas as quetões são as mesmas criadas pelo gestor
-			anexo = anexoDAO.getById(4);
-			byte[] arquivoUpload = anexo.getAnexo();
-			String nomeArquivo = anexo.getNome();
-			
-			FacesContext facesContext = FacesContext.getCurrentInstance();
-			ServletContext scontext = (ServletContext) facesContext.getExternalContext().getContext();
-			String arquivoSalvo = scontext.getRealPath("/img/upload/" + nomeArquivo);
-			criaArquivo(arquivoUpload, arquivoSalvo);//cria o arquivo na pasta da aplicação
-			
-			HttpServletRequest request = (HttpServletRequest) facesContext.getExternalContext().getRequest();
-			String urlAplicacao = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+request.getContextPath();
-			//request.getRequestURL().substring(0,request.getRequestURL().indexOf("/"));
-			
-			emailBean.setUrlQuestionario(request.getRequestURL().toString());
-			emailBean.setUrlArquivoTemplate(urlAplicacao+"/layout/templateEmail.html");
-			emailBean.setUrlArte(urlAplicacao+"/img/upload/" + nomeArquivo);
-			
-			emailUtils.enviaEmailHtml(emailBean);
-			addMessage("Email eviado com sucesso");
-		} catch (Exception e) {
-			e.printStackTrace();
+			for (Questionario quest : questionariosSelecionados) {
+				for (Usuario user : usuariosSelecionados) {
+
+					emailBean.setTo(user.getEmail());
+					quest.getAnexo().setId(4);//TODO remover auto seção
+					anexo = anexoDAO.getById(quest.getAnexo().getId());
+					byte[] arquivoUpload = anexo.getAnexo();
+					String nomeArquivo = anexo.getNome();
+
+					FacesContext facesContext = FacesContext.getCurrentInstance();
+					ServletContext scontext = (ServletContext) facesContext.getExternalContext().getContext();
+					String arquivoSalvo = scontext.getRealPath("/img/upload/" + nomeArquivo);
+					criaArquivo(arquivoUpload, arquivoSalvo);//cria o arquivo na pasta da aplicação
+
+					HttpServletRequest request = (HttpServletRequest) facesContext.getExternalContext().getRequest();
+					String urlAplicacao = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+request.getContextPath();
+					//request.getRequestURL().substring(0,request.getRequestURL().indexOf("/"));
+
+					emailBean.setUrlQuestionario(request.getRequestURL().toString());
+					emailBean.setUrlArquivoTemplate(urlAplicacao+"/layout/templateEmail.html");
+					emailBean.setUrlArte(urlAplicacao+"/img/upload/" + nomeArquivo);
+
+					emailUtils.enviaEmailHtml(emailBean);
+					addMessage("Email eviado com sucesso para: "+user.getNome());
+				}
+			}
+				addMessage("Email(s) eviado(s) com sucesso.");
+			} catch (Exception e) {
+				e.printStackTrace();
+				addMessage("Erro ao tentar enviar e-mail: "+e.getMessage());
+			}
 		}
-	}
-	
-	public void addMessage(String summary) {  
-		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);  
-		FacesContext.getCurrentInstance().addMessage(null, message);  
-	}
 
-	//salva arquivo na pasta da propria aplicacao
-	public void criaArquivo(byte[] bytes, String arquivo) {
-		FileOutputStream fos;
-		try {
-			fos = new FileOutputStream(arquivo);
-			fos.write(bytes);
-			fos.close();
-		} catch (FileNotFoundException ex) {
-			ex.printStackTrace();
-		} catch (IOException ex) {
-			ex.printStackTrace();
+		public void addMessage(String summary) {  
+			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);  
+			FacesContext.getCurrentInstance().addMessage(null, message);  
 		}
-	}
 
-	private SelectItem[] createFilterOptions(String[] data)  {    
-		SelectItem[] options = new SelectItem[data.length + 1];    
+		//salva arquivo na pasta da propria aplicacao
+		public void criaArquivo(byte[] bytes, String arquivo) {
+			FileOutputStream fos;
+			try {
+				fos = new FileOutputStream(arquivo);
+				fos.write(bytes);
+				fos.close();
+			} catch (FileNotFoundException ex) {
+				ex.printStackTrace();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 
-		options[0] = new SelectItem("", "Select");    
-		for(int i = 0; i < data.length; i++) {    
-			options[i + 1] = new SelectItem(data[i], data[i]);    
-		}    
+		private SelectItem[] createFilterOptions(String[] data)  {    
+			SelectItem[] options = new SelectItem[data.length + 1];    
 
-		return options;    
-	}
+			options[0] = new SelectItem("", "Select");    
+			for(int i = 0; i < data.length; i++) {    
+				options[i + 1] = new SelectItem(data[i], data[i]);    
+			}    
 
-	public UsuarioQuestionario getSelectedCar() {  
-		return selectedCar;  
-	}  
+			return options;    
+		}
 
-	public void setSelectedCar(UsuarioQuestionario selectedCar) {  
-		this.selectedCar = selectedCar;  
-	}  
-
-	public LazyDataModel<UsuarioQuestionario> getLazyModel() {  
-		return lazyModel;  
-	}  
-
-	private void populateRandomCars(List<UsuarioQuestionario> list, int size) {  
-		for(int i = 0 ; i < size ; i++) {  
-			list.add(new UsuarioQuestionario(getRandomModel(), getRandomYear(), getRandomManufacturer(), getRandomColor()));  
+		public UsuarioQuestionario getSelectedCar() {  
+			return selectedCar;  
 		}  
-	}  
 
-	private String getRandomModel() {
+		public void setSelectedCar(UsuarioQuestionario selectedCar) {  
+			this.selectedCar = selectedCar;  
+		}  
 
-		return String.valueOf((Math.random() * 100))  ;
+		public LazyDataModel<UsuarioQuestionario> getLazyModel() {  
+			return lazyModel;  
+		}  
+
+		private void populateRandomCars(List<UsuarioQuestionario> list, int size) {  
+			for(int i = 0 ; i < size ; i++) {  
+				list.add(new UsuarioQuestionario(getRandomModel(), getRandomYear(), getRandomManufacturer(), getRandomColor()));  
+			}  
+		}  
+
+		private String getRandomModel() {
+
+			return String.valueOf((Math.random() * 100))  ;
+		}
+
+		private String getRandomColor() {  
+			return colors[(int) (Math.random() * 10)];  
+		}  
+
+		private String getRandomManufacturer() {  
+			return manufacturers[(int) (Math.random() * 10)];  
+		}  
+
+		private int getRandomYear() {  
+			return (int) (Math.random() * 50 + 1960);  
+		}
+
+		public void onRowSelect(){
+
+		}
+
+		public List<Usuario> getListControleEmail() {
+			return listControleEmail;
+		}
+
+		public void setListControleEmail(List<Usuario> listControleEmail) {
+			this.listControleEmail = listControleEmail;
+		}
+
+		public LazyDataModel<Usuario> getLazyModelCE() {
+			return lazyModelCE;
+		}
+
+		public void setLazyModelCE(LazyDataModel<Usuario> lazyModelCE) {
+			this.lazyModelCE = lazyModelCE;
+		}
+
+		public Usuario getSelectedEC() {
+			return selectedEC;
+		}
+
+		public void setSelectedEC(Usuario selectedEC) {
+			this.selectedEC = selectedEC;
+		}
+
+		public List<Usuario> getFilteredCars() {
+			return filteredCars;
+		}
+
+		public void setFilteredCars(List<Usuario> filteredCars) {
+			this.filteredCars = filteredCars;
+		}
+
+		/**
+		 * @return the questionariosSelecionados
+		 */
+		public List<Questionario> getQuestionariosSelecionados() {
+			return questionariosSelecionados;
+		}
+
+		/**
+		 * @param questionariosSelecionados the questionariosSelecionados to set
+		 */
+		public void setQuestionariosSelecionados(
+				List<Questionario> questionariosSelecionados) {
+			this.questionariosSelecionados = questionariosSelecionados;
+		}
+
+		/**
+		 * @return the usuarioQuestionario
+		 */
+		public List<UsuarioQuestionario> getUsuarioQuestionario() {
+			return usuarioQuestionario;
+		}
+
+		/**
+		 * @param usuarioQuestionario the usuarioQuestionario to set
+		 */
+		public void setUsuarioQuestionario(List<UsuarioQuestionario> usuarioQuestionario) {
+			this.usuarioQuestionario = usuarioQuestionario;
+		}
+
+		/**
+		 * @return the usuariosSelecionados
+		 */
+		public List<Usuario> getUsuariosSelecionados() {
+			return usuariosSelecionados;
+		}
+
+		/**
+		 * @param usuariosSelecionados the usuariosSelecionados to set
+		 */
+		public void setUsuariosSelecionados(List<Usuario> usuariosSelecionados) {
+			this.usuariosSelecionados = usuariosSelecionados;
+		}
+
+		/**
+		 * @param lazyModel the lazyModel to set
+		 */
+		public void setLazyModel(LazyDataModel<UsuarioQuestionario> lazyModel) {
+			this.lazyModel = lazyModel;
+		}
 	}
-
-	private String getRandomColor() {  
-		return colors[(int) (Math.random() * 10)];  
-	}  
-
-	private String getRandomManufacturer() {  
-		return manufacturers[(int) (Math.random() * 10)];  
-	}  
-
-	private int getRandomYear() {  
-		return (int) (Math.random() * 50 + 1960);  
-	}
-
-	public void onRowSelect(){
-
-	}
-
-	public List<Usuario> getListControleEmail() {
-		return listControleEmail;
-	}
-
-	public void setListControleEmail(List<Usuario> listControleEmail) {
-		this.listControleEmail = listControleEmail;
-	}
-
-	public LazyDataModel<Usuario> getLazyModelCE() {
-		return lazyModelCE;
-	}
-
-	public void setLazyModelCE(LazyDataModel<Usuario> lazyModelCE) {
-		this.lazyModelCE = lazyModelCE;
-	}
-
-	public Usuario getSelectedEC() {
-		return selectedEC;
-	}
-
-	public void setSelectedEC(Usuario selectedEC) {
-		this.selectedEC = selectedEC;
-	}
-
-	public List<Usuario> getFilteredCars() {
-		return filteredCars;
-	}
-
-	public void setFilteredCars(List<Usuario> filteredCars) {
-		this.filteredCars = filteredCars;
-	}
-
-	/**
-	 * @return the questionariosSelecionados
-	 */
-	public List<Questionario> getQuestionariosSelecionados() {
-		return questionariosSelecionados;
-	}
-
-	/**
-	 * @param questionariosSelecionados the questionariosSelecionados to set
-	 */
-	public void setQuestionariosSelecionados(
-			List<Questionario> questionariosSelecionados) {
-		this.questionariosSelecionados = questionariosSelecionados;
-	}
-
-	/**
-	 * @return the usuarioQuestionario
-	 */
-	public List<UsuarioQuestionario> getUsuarioQuestionario() {
-		return usuarioQuestionario;
-	}
-
-	/**
-	 * @param usuarioQuestionario the usuarioQuestionario to set
-	 */
-	public void setUsuarioQuestionario(List<UsuarioQuestionario> usuarioQuestionario) {
-		this.usuarioQuestionario = usuarioQuestionario;
-	}
-
-	/**
-	 * @return the usuariosSelecionados
-	 */
-	public List<Usuario> getUsuariosSelecionados() {
-		return usuariosSelecionados;
-	}
-
-	/**
-	 * @param usuariosSelecionados the usuariosSelecionados to set
-	 */
-	public void setUsuariosSelecionados(List<Usuario> usuariosSelecionados) {
-		this.usuariosSelecionados = usuariosSelecionados;
-	}
-
-	/**
-	 * @param lazyModel the lazyModel to set
-	 */
-	public void setLazyModel(LazyDataModel<UsuarioQuestionario> lazyModel) {
-		this.lazyModel = lazyModel;
-	}
-}
