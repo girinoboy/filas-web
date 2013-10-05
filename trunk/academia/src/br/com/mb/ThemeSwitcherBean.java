ef6f1b4905001ef1c0679f4a5397877c6a0fa5f3
girinoboy <girinoboy@4a19f9eb-a919-de0c-50ec-919b7c35c86e>
2013-10-05 10:38:54 +0000

    trocar de tema- versão de teste

--- trunk/academia/src/br/com/mb/ThemeSwitcherBean.java
+++ trunk/academia/src/br/com/mb/ThemeSwitcherBean.java
@@ -29,39 +29,40 @@ public class ThemeSwitcherBean extends GenericoMB{
     @PostConstruct
     public void init() {
         theme = gp.getTheme();
-        //TODO mudar para pegar valores do banco de dados.  
-        themes = new TreeMap<String, String>();  
-        themes.put("Aristo", "aristo");  
-        themes.put("Black-Tie", "black-tie");  
-        themes.put("Blitzer", "blitzer");  
-        themes.put("Bluesky", "bluesky");  
-        themes.put("Casablanca", "casablanca");  
-        themes.put("Cupertino", "cupertino");  
-        themes.put("Dark-Hive", "dark-hive");  
-        themes.put("Dot-Luv", "dot-luv");  
-        themes.put("Eggplant", "eggplant");  
-        themes.put("Excite-Bike", "excite-bike");  
-        themes.put("Flick", "flick");  
-        themes.put("Glass-X", "glass-x");  
-        themes.put("Hot-Sneaks", "hot-sneaks");  
-        themes.put("Humanity", "humanity");  
-        themes.put("Le-Frog", "le-frog");  
-        themes.put("Midnight", "midnight");  
-        themes.put("Mint-Choc", "mint-choc");  
-        themes.put("Overcast", "overcast");  
-        themes.put("Pepper-Grinder", "pepper-grinder");  
-        themes.put("Redmond", "redmond");  
-        themes.put("Rocket", "rocket");  
-        themes.put("Sam", "sam");  
-        themes.put("Smoothness", "smoothness");  
-        themes.put("South-Street", "south-street");  
-        themes.put("Start", "start");  
-        themes.put("Sunny", "sunny");  
-        themes.put("Swanky-Purse", "swanky-purse");  
-        themes.put("Trontastic", "trontastic");  
-        themes.put("UI-Darkness", "ui-darkness");  
-        themes.put("UI-Lightness", "ui-lightness");  
-        themes.put("Vader", "vader");  
+        //TODO mudar para pegar valores do banco de dados.
+        themes = new TreeMap<String, String>();
+        themes.put("Aristo", "aristo");
+        themes.put("Black-Tie", "black-tie");
+        themes.put("Blitzer", "blitzer");
+        themes.put("Bluesky", "bluesky");
+        themes.put("Bootstrap", "bootstrap");
+        themes.put("Casablanca", "casablanca");
+        themes.put("Cupertino", "cupertino");
+        themes.put("Dark-Hive", "dark-hive");
+        themes.put("Dot-Luv", "dot-luv");
+        themes.put("Eggplant", "eggplant");
+        themes.put("Excite-Bike", "excite-bike");
+        themes.put("Flick", "flick");
+        themes.put("Glass-X", "glass-x");
+        themes.put("Hot-Sneaks", "hot-sneaks");
+        themes.put("Humanity", "humanity");
+        themes.put("Le-Frog", "le-frog");
+        themes.put("Midnight", "midnight");
+        themes.put("Mint-Choc", "mint-choc");
+        themes.put("Overcast", "overcast");
+        themes.put("Pepper-Grinder", "pepper-grinder");
+        themes.put("Redmond", "redmond");
+        themes.put("Rocket", "rocket");
+        themes.put("Sam", "sam");
+        themes.put("Smoothness", "smoothness");
+        themes.put("South-Street", "south-street");
+        themes.put("Start", "start");
+        themes.put("Sunny", "sunny");
+        themes.put("Swanky-Purse", "swanky-purse");
+        themes.put("Trontastic", "trontastic");
+        themes.put("UI-Darkness", "ui-darkness");
+        themes.put("UI-Lightness", "ui-lightness");
+        themes.put("Vader", "vader");
     }
       
     public void saveTheme() throws HibernateException, Exception {
