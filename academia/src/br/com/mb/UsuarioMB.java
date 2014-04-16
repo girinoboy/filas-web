d42735f3daddcd86685575a4ca3cb1f8696bb2ef
girinoboy <girinoboy@gmail.com>
2014-04-16 20:39:28 +0000

    organização de imports

--- academia/src/br/com/mb/UsuarioMB.java
+++ academia/src/br/com/mb/UsuarioMB.java
@@ -14,11 +14,8 @@ import java.util.List;
 import java.util.Map;
 
 import javax.faces.FacesException;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
-import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.imageio.stream.FileImageOutputStream;
